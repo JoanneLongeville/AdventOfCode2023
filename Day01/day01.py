d38bc58ea183c885dfffbbc0199bdf18eb3ec641
Joanne Longeville <joanne.longeville@gmail.com>
2024-11-18 17:03:47 +0100

    add part two day one

--- Day01/day01.py
+++ Day01/day01.py
@@ -45,3 +45,56 @@ def part_one(word):
 
 print('test part01:', test_part_one(word))
 print('part one:', part_one(word))
+
+
+# Part02
+
+# 29 83 13 24 42 14 76 = 281
+word = (
+    "two1nine eightwothree abcone2threexyz xtwone3four "
+    "4nineeightseven2 zoneight234 7pqrstsixteen"
+)
+word_to_digit = {
+    "one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
+    "six": "6", "seven": "7", "eight": "8", "nine": "9"
+}
+
+
+def transform_char_to_digit(word_split):
+    def transform_word(word):
+        result = ""
+        i = 0
+        while i < len(word):
+            match_found = False
+            for key in word_to_digit:
+                if i + len(key) <= len(word) and word[i:i+len(key)] == key:
+                    result += word_to_digit[key]
+                    i += len(key)
+                    match_found = True
+                    break
+            if not match_found:
+                result += word[i]
+                i += 1
+        return result
+    return [transform_word(word) for word in word_split]
+
+
+def test_part_two(word):
+    word = word_split(word)
+    print(word)
+    word_to_digit = transform_char_to_digit(word)
+    print(word_to_digit)
+    digit = find_first_and_last_digit(word_to_digit)
+    return sum_first_and_last_digit(digit)
+
+
+def part_two(word):
+    word = import_yml('input.yml')
+    word = word_split(word)
+    word_to_digit = transform_char_to_digit(word)
+    digit = find_first_and_last_digit(word_to_digit)
+    return sum_first_and_last_digit(digit)
+
+
+print('test part02:', test_part_two(word))
+print('part two:', part_two(word))  # still not working (need 54591 but have 54623)
